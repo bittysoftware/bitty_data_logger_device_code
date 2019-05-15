65dfea390caefb756f7141a3a88578ab538f4f2e
Martin Woolley <martin@woolleynet.com>
2019-05-15 07:05:05 +0100

    Added sample for automatic accelerometer data capture over Bluetooth

---
+++ microbit/makecode/bdl_auto_accelerometer.js
@@ -0,0 +1,10 @@
+let connected = 0
+bluetooth.onBluetoothConnected(function () {
+    connected = 1
+    basic.showString("C")
+})
+bluetooth.onBluetoothDisconnected(function () {
+    connected = 0
+    basic.showString("D")
+})
+bluetooth.startAccelerometerService()
